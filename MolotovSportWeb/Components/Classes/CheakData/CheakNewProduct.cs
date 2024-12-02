df08d314deceafa53c7518b86ff0468924c8b95e
VirtoriaECTS <vikmolotova@gmail.com>
2024-12-02 22:58:52 +0500

    добавление товара

---
+++ MolotovSportWeb/Components/Classes/CheakData/CheakNewProduct.cs
@@ -0,0 +1,31 @@
+﻿namespace MolotovSportWeb.Components.Classes.CheakData
+{
+    public class CheakNewProduct
+    {
+        public bool cheakPrice(string price)
+        {
+            try
+            {
+                int number = Convert.ToInt32(price);
+                if(number <= 0) return false;
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool cheakName(string name)
+        {
+            if(name.Length > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
--- MolotovSportWeb/Components/Classes/DataBase/AddInfo.cs
+++ MolotovSportWeb/Components/Classes/DataBase/AddInfo.cs
@@ -9,8 +9,81 @@
         {
             using (var context = new MolotovSportWebContext())
             {
+                var product = new Product
+                {
+                    ProductName = name,
+                    FirmId = IdFirm,
+                    CategoryId = IdCaregory,
+                    CategoryIdMini = IdCaregoryMini,
+                    GenderId = idGender,
+                    ImageUrl = image,
+                    Price = price,
+                };
 
-                
+                context.Products.Add(product);
+                context.SaveChanges();
+
+                var productNew = context.Products.Where(p => p.ProductName == name).Where(p => p.FirmId == IdFirm).Where(p => p.CategoryId == IdCaregory).Where(p => p.GenderId == idGender)
+                    .Where(p => p.ImageUrl == image).Where(p => p.Price == price).FirstOrDefault();
+
+
+                foreach (string item in Size)
+                {
+                    var sizeNew = new ProductSize
+                    {
+                        ProductId = productNew.ProductId,
+                        Size = item,
+                        Count = 1
+                    };
+                    context.Add(sizeNew);
+                }
+
+                context.SaveChanges();
+            }
+        }
+
+        //Дописать удаление
+        //public void DeleteFirm(int idFirm)
+        //{
+        //    using (var context = new MolotovSportWebContext())
+        //    {
+        //        var listProduct = context.Products.Where(p => p.FirmId == idFirm).ToList();
+
+        //        foreach (var product in listProduct)
+        //        {
+        //            foreach (var size in product.ProductSizes)
+        //            {
+        //                size.Count = 0;
+        //            }
+        //        }
+        //        context.SaveChanges();
+        //    }
+
+        //}
+
+        public void AddFirm(string nameFirm)
+        {
+            using (var context = new MolotovSportWebContext())
+            {
+                var firm = new Firm
+                {
+                    FirmName = nameFirm,
+                };
+
+                context.SaveChanges();
+            }
+        }
+
+        public void AddCategoryMini(int idCategory, string categoryMiniNameText)
+        {
+            using (var context = new MolotovSportWebContext())
+            {
+                var categoryMini = new CategoriesMini
+                {
+                    CategoryMiniName = categoryMiniNameText,
+                };
+
+                context.SaveChanges();
             }
         }
     }
