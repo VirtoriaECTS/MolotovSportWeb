7f6db311ee4c80cd639ac05a5f4107471f891141
VirtoriaECTS <vikmolotova@gmail.com>
2025-01-19 17:05:12 +0500

    вывод диаграммы

---
+++ MolotovSportWeb/Components/Classes/Statistic/Statistic.cs
@@ -0,0 +1,25 @@
+﻿using MolotovSportWeb.Models;
+
+namespace MolotovSportWeb.Components.Classes.Statistic
+{
+    public class Statistic
+    {
+        public List<double> priceMonth(DateTime selectMonth)
+        {
+            List<double> money = new List<double>();
+            using (var context = new MolotovSportWebContext())
+            {
+
+                var order = context.Orders.Where(x => x.OrderData > selectMonth).GroupBy(x => x.OrderData);
+
+
+                foreach(var day in order)
+                {
+                    double sum = day.Sum(x => x.TotalAmout);
+                    money.Add(sum);
+                }
+            }
+            return money;
+        }
+    }
+}
--- MolotovSportWeb/Program.cs
+++ MolotovSportWeb/Program.cs
@@ -7,6 +7,7 @@ using MudBlazor.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
+
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
